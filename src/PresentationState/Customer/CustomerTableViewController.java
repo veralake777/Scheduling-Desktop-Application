672aa3337e03e2cddf65d2adc2595572aaba058f
VeraButler <verabutlerdesign@gmail.com>
2020-03-10 12:09:04 -0400

    add skeleton for tableviews

--- src/PresentationState/Customer/CustomerTableViewController.java
+++ src/PresentationState/Customer/CustomerTableViewController.java
@@ -1,4 +1,10 @@
 package PresentationState.Customer;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
 public class CustomerTableViewController {
+    public Button addNew;
+    public TableColumn customer;
+    public TableColumn consultant;
 }
